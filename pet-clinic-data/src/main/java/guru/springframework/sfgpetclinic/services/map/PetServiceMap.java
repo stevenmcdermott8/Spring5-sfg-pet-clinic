950a7479b95bae435b80bd91faec0bdcac4a49c0
Steven McDermott <stevmc@vsp.com>
2020-02-11 20:17:34 -0800

    Implement map based services, Closes #6.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+/**
+ * stevmc created on 2/11/20
+ */
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+	@Override
+	public Set<Pet> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		super.deleteByObject(object);
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		return super.findById(id);
+	}
+}
