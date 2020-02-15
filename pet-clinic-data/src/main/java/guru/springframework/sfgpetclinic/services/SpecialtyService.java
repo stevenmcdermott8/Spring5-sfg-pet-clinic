7126f1f28d7a2006a2dec3a8672fc8f19f4d8869
Steven McDermott <stevmc@vsp.com>
2020-02-15 14:49:32 -0800

    Create Specialty Map Service, Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+/**
+ * stevmc created on 2/15/20
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * stevmc created on 2/15/20
+ */
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+	@Override
+	public Set<Specialty> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public Specialty save(Specialty object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(Specialty object) {
+		super.deleteByObject(object);
+	}
+
+	@Override
+	public Specialty findById(Long id) {
+		return super.findById(id);
+	}
+}
