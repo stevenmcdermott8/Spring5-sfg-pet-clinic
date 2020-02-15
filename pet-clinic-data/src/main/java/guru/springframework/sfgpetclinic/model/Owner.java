189447ef303df752de6ff5aace3fe396c73a9cb3
Steven McDermott <stevmc@vsp.com>
2020-02-15 12:09:48 -0800

    added visits entity, updated owner with pet relationship, Closes #26.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * stevmc created on 2/10/20
  */
 public class Owner extends Person {
+
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 }
