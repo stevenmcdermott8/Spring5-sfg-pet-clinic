1d0d989c0aa48e0fb006835bcc3d0f1bda1e2487
Steven McDermott <stevmc@vsp.com>
2020-02-17 07:34:12 -0800

    Implement Vet entity and related objects to be JPA entities, Closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,11 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * stevmc created on 2/15/20
  */
+@Entity
+@Table(name = "specialities")
 public class Specialty extends BaseEntity {
 
-	private String description;
+	@Column(name = "description") private String description;
 
 	public String getDescription() {
 		return description;
